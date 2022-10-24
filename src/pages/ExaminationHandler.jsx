8d009a092ac69ae3092a01fbd23c807a31ce6cab
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-10-24 17:19:57 +0100

    made changes

--- src/pages/ExaminationHandler.jsx
+++ src/pages/ExaminationHandler.jsx
@@ -16,10 +16,10 @@ import { pink } from "@mui/material/colors";
 import { FavoriteBorder, Favorite } from "@mui/icons-material";
 import { httpService } from "../httpService";
 import { useState, useEffect } from "react";
-import { Spinner, Table } from "react-bootstrap";
+import { Spinner } from "react-bootstrap";
 import Swal from "sweetalert2";
 import DataTable from "react-data-table-component";
-import { Badge } from "react-bootstrap";
+import { Badge, Modal } from "react-bootstrap";
 
 export default function ExaminationHandler() {
   const [data, setData] = useState([]);
@@ -27,6 +27,10 @@ export default function ExaminationHandler() {
   const [processLoading, setProcessLoading] = useState(false);
 
   const [examination, setExamination] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const createNewExamination = async (e) => {
     e.preventDefault();
@@ -59,6 +63,14 @@ export default function ExaminationHandler() {
     }
   };
 
+  const expandableComponent = ({ data }) => {
+    return (
+      <div className="p-2">
+        <Button onClick={handleShow}>Update subjects</Button>
+      </div>
+    );
+  };
+
   useEffect(() => {
     viewCreatedExaminations();
   }, []);
@@ -87,7 +99,6 @@ export default function ExaminationHandler() {
               <Typography fontWeight={600} variant="h4" gutterBottom>
                 Examination Control
               </Typography>
-              <hr />
             </div>
             <div>{processLoading ? <Spinner animation="grow" /> : null}</div>
           </div>
@@ -113,11 +124,33 @@ export default function ExaminationHandler() {
           </div>
           <div className="mt-2">
             <div className="mt-2">
-              <DataTable data={data} columns={columns} pagination />
+              <DataTable
+                data={data}
+                columns={columns}
+                pagination
+                expandableRows
+                expandableRowsComponent={expandableComponent}
+              />
             </div>
           </div>
         </div>
       </div>
+      <>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Subjects</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     </div>
   );
 }
