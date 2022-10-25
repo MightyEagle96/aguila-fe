9c9d978a39e3276fa0c00ae50b1d19efd8e8d1d1
Mighty Eagle <mightyeaglecorp@gmail.com>
2022-10-26 00:07:38 +0100

    made changes

--- src/pages/centres/CentreCard.jsx
+++ src/pages/centres/CentreCard.jsx
@@ -1,5 +1,5 @@
 import { People } from "@mui/icons-material";
-import { Avatar, Icon, Typography } from "@mui/material";
+import { Avatar, CardActionArea, Icon, Typography } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { httpService } from "../../httpService";
 
@@ -20,18 +20,23 @@ export default function CentreCard({ data }) {
   }, []);
 
   return (
-    <div className="col-md-3">
-      <div className="p-2 border rounded me-1 mb-1">
-        <Typography fontWeight={600} gutterBottom>
-          {data.title}
-        </Typography>
-        <div className="d-flex justify-content-between">
-          <Icon sx={{ color: "#00a152" }}>
-            <People />
-          </Icon>
-          <Avatar sx={{ backgroundColor: "#ab003c" }}>{length || 0}</Avatar>
+    <div className="col-md-3 me-2 mb-2">
+      <CardActionArea
+        sx={{ backgroundColor: "#eeeeee" }}
+        onClick={() => window.location.assign("/registrations/" + data._id)}
+      >
+        <div className="p-2">
+          <Typography fontWeight={600} gutterBottom>
+            {data.title}
+          </Typography>
+          <div className="d-flex justify-content-between">
+            <Icon sx={{ color: "#00a152" }}>
+              <People />
+            </Icon>
+            <Avatar sx={{ backgroundColor: "#ab003c" }}>{length || 0}</Avatar>
+          </div>
         </div>
-      </div>
+      </CardActionArea>
     </div>
   );
 }
--- src/pages/registrations/ViewExamRegistrations.jsx
+++ src/pages/registrations/ViewExamRegistrations.jsx
@@ -1,12 +1,14 @@
-import { Skeleton, Stack, Typography } from "@mui/material";
+import { Button, Skeleton, Stack, TextField, Typography } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import DataTable from "react-data-table-component";
 import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 import { httpService } from "../../httpService";
 
 function ViewExamRegistrations() {
   const [data, setData] = useState(null);
   const { id } = useParams();
+  const [limit, setLimit] = useState(0);
 
   const getData = async () => {
     const path = `registrations/${id}`;
@@ -22,6 +24,24 @@ function ViewExamRegistrations() {
     getData();
   }, []);
 
+  const createDummyCandidates = (e) => {
+    e.preventDefault();
+    Swal.fire({
+      icon: "question",
+      title: "Please confirm",
+      text: "Do you wish to create dummy candidates for this examination?",
+      showCancelButton: true,
+    }).then(async () => {
+      const path = "createMultipleCandidates";
+
+      const res = await httpService.post(path, { limit, examination: id });
+
+      if (res) {
+        Swal.fire({ icon: "success", title: "SUCCESS", text: res.data });
+        getData();
+      }
+    });
+  };
   const expandableComponent = ({ data }) => {
     return (
       <div className="alert alert-success">
@@ -77,14 +97,34 @@ function ViewExamRegistrations() {
       ),
     },
     {
-      name: "Score",
+      name: "Total Score",
       selector: (row) => (
         <Typography variant="body1" textTransform={"uppercase"}>
-          {row.score}
+          {row.totalScore}
         </Typography>
       ),
     },
   ];
+
+  const deleteAllRegistrations = () => {
+    Swal.fire({
+      icon: "question",
+      title: "Please Confirm",
+      text: "Do you want to delete all candidates? This cannot be reversed",
+      showCancelButton: true,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const path = `deleteCandidates/${id}`;
+
+        const res = await httpService.delete(path);
+
+        if (res) {
+          getData();
+          Swal.fire({ icon: "success", text: res.data });
+        }
+      }
+    });
+  };
   return (
     <div>
       <div className="">
@@ -95,6 +135,36 @@ function ViewExamRegistrations() {
                 <Typography variant="h4" fontWeight={600}>
                   {data.examination.title} Data dashboard
                 </Typography>
+              </div>
+              <div className="mb-4">
+                <div className="d-flex justify-content-end">
+                  <div className="row">
+                    <div className="col-md-6">
+                      <form onSubmit={createDummyCandidates}>
+                        <div>
+                          <TextField
+                            label="Limit"
+                            type="number"
+                            name="limit"
+                            onChange={(e) => setLimit(e.target.value)}
+                          />
+                        </div>
+                        <div className="">
+                          <Button type="submit">Create dummy Candidates</Button>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="col-md-6">
+                      <Button
+                        color="error"
+                        variant="outlined"
+                        onClick={deleteAllRegistrations}
+                      >
+                        Delete all registrations
+                      </Button>
+                    </div>
+                  </div>
+                </div>
               </div>
               <DataTable
                 title={
