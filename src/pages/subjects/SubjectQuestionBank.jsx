a086fe4668ff7a197a067fb78675e4ef70aaab08
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2023-04-11 14:04:55 +0100

    added the upload question component

--- src/pages/subjects/SubjectQuestionBank.jsx
+++ src/pages/subjects/SubjectQuestionBank.jsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { httpService } from "../../httpService";
 import { CircularProgress, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { AlertContext } from "../../contexts/AlertContext";
+
 export default function SubjectQuestionBank() {
   const { id } = useParams();
+  const { setAlertData } = useContext(AlertContext);
   const [questionBank, setQuestionBank] = useState(null);
   const [loading, setLoading] = useState(false);
-
+  const [questionFile, setQuestionFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
   const getQuestionBank = async () => {
     setLoading(true);
     const { data } = await httpService(
@@ -22,6 +27,29 @@ export default function SubjectQuestionBank() {
   useEffect(() => {
     getQuestionBank();
   }, []);
+
+  const handleFileUpload = async (e) => {
+    e.preventDefault();
+
+    setUploading(true);
+    let formData = new FormData();
+
+    formData.append("questionFile", questionFile, questionFile.name);
+    const path = `uploadQuestionFile/${id}`;
+    const { data, error } = await httpService.post(path, formData);
+
+    if (error) {
+      setAlertData({
+        message: error,
+        severity: "error",
+        setMessage: error,
+        open: true,
+      });
+    }
+
+    setQuestionFile(null);
+    setUploading(false);
+  };
   return (
     <div className="mt-5 mb-5 p-3">
       {loading && <CircularProgress />}
@@ -38,6 +66,36 @@ export default function SubjectQuestionBank() {
             >
               {questionBank.subject.name}
             </Typography>
+          </div>
+          <div className="col-lg-4">
+            {" "}
+            <div>
+              <div>
+                <form encType="multipart/form-data" onSubmit={handleFileUpload}>
+                  <label for="formFile" class="form-label">
+                    Upload examination questions
+                  </label>
+                  <input
+                    class="form-control"
+                    type="file"
+                    id="questionFile"
+                    accept=".xlsx, .csv"
+                    name="questionFile"
+                    onChange={(e) => setQuestionFile(e.target.files[0])}
+                  />
+                  <LoadingButton
+                    type="submit"
+                    className="mt-2"
+                    endIcon={<i class="fas fa-file-excel    "></i>}
+                    loading={uploading}
+                    loadingPosition="end"
+                    disabled={!questionFile}
+                  >
+                    upload questions
+                  </LoadingButton>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       )}
