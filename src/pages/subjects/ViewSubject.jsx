9add7254c2696a514c9dda1edd8572e629f3c0c5
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2023-04-11 11:15:41 +0100

    added question bank

--- src/pages/subjects/ViewSubject.jsx
+++ src/pages/subjects/ViewSubject.jsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { httpService } from "../../httpService";
 import { CircularProgress, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { AddTask } from "@mui/icons-material";
+import { AlertContext } from "../../contexts/AlertContext";
+import { Table } from "react-bootstrap";
 
 export default function ViewSubject() {
+  const { setAlertData } = useContext(AlertContext);
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
-
+  const [questionBanks, setQuestionBanks] = useState([]);
   const [subject, setSubject] = useState(null);
+  const [creating, setCreating] = useState(false);
 
   const getSubject = async () => {
     setLoading(true);
@@ -19,8 +25,39 @@ export default function ViewSubject() {
     setLoading(false);
   };
 
+  const addQuestionBank = async () => {
+    setCreating(true);
+    const { data, error } = await httpService(
+      `aguila/subject/questionbank/add/${id}`
+    );
+
+    if (data) {
+      viewQuestionBanks();
+      setAlertData({ message: data, severity: "success", open: true });
+    }
+    if (error) {
+      setAlertData({ message: error, severity: "error", open: true });
+    }
+    setCreating(false);
+  };
+  const viewQuestionBanks = async () => {
+    setLoading(true);
+    const { data, error } = await httpService(
+      `aguila/subject/questionbank/view/${id}`
+    );
+
+    if (data) {
+      setQuestionBanks(data);
+    }
+    if (error) {
+      setAlertData({ message: error, severity: "error", open: true });
+    }
+    setLoading(false);
+  };
+
   useEffect(() => {
     getSubject();
+    viewQuestionBanks();
   }, []);
   return (
     <div className="mt-5 mb-5 p-3">
@@ -36,6 +73,34 @@ export default function ViewSubject() {
             >
               {subject.name}
             </Typography>
+          </div>
+          <div className="mt-2">
+            <LoadingButton
+              endIcon={<AddTask />}
+              loadingPosition="end"
+              onClick={addQuestionBank}
+              loading={creating}
+            >
+              add question bank
+            </LoadingButton>
+          </div>
+          <div className="mt-2 col-lg-4">
+            <Table bordered>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {questionBanks.map((c, i) => (
+                  <tr>
+                    <td>
+                      <Typography>Question bank {i + 1}</Typography>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
           </div>
         </>
       )}
