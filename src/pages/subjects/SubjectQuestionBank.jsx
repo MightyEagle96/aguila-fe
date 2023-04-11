9b10cc50c90b5de6bcfcfe8e2b9066db779df6b4
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2023-04-11 13:03:13 +0100

    added the view question bank

--- src/pages/subjects/SubjectQuestionBank.jsx
+++ src/pages/subjects/SubjectQuestionBank.jsx
@@ -1,5 +1,46 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { httpService } from "../../httpService";
+import { CircularProgress, Typography } from "@mui/material";
 export default function SubjectQuestionBank() {
-  return <div>SubjectQuestionBank</div>;
+  const { id } = useParams();
+  const [questionBank, setQuestionBank] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getQuestionBank = async () => {
+    setLoading(true);
+    const { data } = await httpService(
+      `aguila/subject/questionbank/findone/${id}`
+    );
+
+    if (data) {
+      setQuestionBank(data);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getQuestionBank();
+  }, []);
+  return (
+    <div className="mt-5 mb-5 p-3">
+      {loading && <CircularProgress />}
+      {questionBank && (
+        <div>
+          <div className="alert alert-light col-lg-6 shadow-sm">
+            <Typography variant="caption" gutterBottom>
+              Question bank for
+            </Typography>
+            <Typography
+              textTransform={"uppercase"}
+              variant="h4"
+              fontWeight={700}
+            >
+              {questionBank.subject.name}
+            </Typography>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
