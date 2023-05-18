ce00d8c1d172f4ff394ada417d53ab095b8ee144
EDS <mightyeaglecorp@gmail.com>
2023-05-18 14:49:18 +0100

    made changes

--- src/pages/examinations/ExaminationSchedule.jsx
+++ src/pages/examinations/ExaminationSchedule.jsx
@@ -1,8 +1,12 @@
-import { CircularProgress, Typography } from "@mui/material";
+import { Button, CircularProgress, TextField, Typography } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { httpService } from "../../httpService";
 import ExamSessionModelComponent from "../../components/ExamSessionModelComponent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AddComment } from "@mui/icons-material";
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
+import { LoadingButton } from "@mui/lab";
 export default function ExaminationSchedule() {
   const { id } = useParams();
   const [examination, setExamination] = useState(null);
@@ -12,6 +16,8 @@ export default function ExaminationSchedule() {
     assigned: 0,
     unassigned: 0,
   });
+
+  const [showInstructionField, setShowInstructionField] = useState(false);
 
   const getExamination = async () => {
     setLoading(true);
@@ -46,42 +52,75 @@ export default function ExaminationSchedule() {
         {loading && <CircularProgress />}
         {examination ? (
           <div>
-            <Typography
-              variant="h4"
-              textTransform={"capitalize"}
-              fontWeight={600}
-            >
-              {examination.title} examination schedule
-            </Typography>
+            <div className="row">
+              <div className="col-lg-9">
+                <Typography
+                  variant="h4"
+                  textTransform={"capitalize"}
+                  fontWeight={600}
+                >
+                  {examination.title} schedule
+                </Typography>
 
-            <div className="row mt-3">
-              <div
-                className="col-lg-3 p-3"
-                style={{ backgroundColor: "#fdb874" }}
-              >
-                <Typography>
-                  Total Candidates: {analysis.total.toLocaleString()}
-                </Typography>
+                <div className="row mt-3">
+                  <div
+                    className="col-lg-3 p-3"
+                    style={{ backgroundColor: "#fdb874" }}
+                  >
+                    <Typography>
+                      Total Candidates: {analysis.total.toLocaleString()}
+                    </Typography>
+                  </div>
+                  <div
+                    className="col-lg-3 p-3"
+                    style={{ backgroundColor: "#b088ad", color: "white" }}
+                  >
+                    <Typography>
+                      Unassigned: {analysis.unassigned.toLocaleString()}
+                    </Typography>
+                  </div>
+                  <div
+                    className="col-lg-3 p-3"
+                    style={{ backgroundColor: "#1d2b67", color: "white" }}
+                  >
+                    <Typography>
+                      Assigned: {analysis.assigned.toLocaleString()}
+                    </Typography>
+                  </div>
+                </div>
+                <div className="mt-3">
+                  <ExamSessions examination={examination} />
+                </div>
               </div>
-              <div
-                className="col-lg-3 p-3"
-                style={{ backgroundColor: "#b088ad", color: "white" }}
-              >
-                <Typography>
-                  Unassigned: {analysis.unassigned.toLocaleString()}
+              <div className="col-lg-3 shadow-sm">
+                <Typography textTransform={"uppercase"} variant="h6">
+                  Instructions for this examination
                 </Typography>
+                <div className="mt-2">
+                  {examination.instructions.length === 0 && (
+                    <Typography variant="subtitle1" color="GrayText">
+                      No instructions yet
+                    </Typography>
+                  )}
+
+                  <Button
+                    startIcon={<AddComment />}
+                    onClick={() =>
+                      setShowInstructionField(!showInstructionField)
+                    }
+                  >
+                    Add Instruction
+                  </Button>
+                  {showInstructionField && (
+                    <div className="mt-2">
+                      <TextField fullWidth multiline maxRows={6} />
+                      <div className="mt-2">
+                        <LoadingButton>update</LoadingButton>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
-              <div
-                className="col-lg-3 p-3"
-                style={{ backgroundColor: "#1d2b67", color: "white" }}
-              >
-                <Typography>
-                  Assigned: {analysis.assigned.toLocaleString()}
-                </Typography>
-              </div>
-            </div>
-            <div className="mt-3">
-              <ExamSessions examination={examination} />
             </div>
           </div>
         ) : null}
