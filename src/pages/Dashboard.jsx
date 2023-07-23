1ed59ccc9e51dda8b4e6920f27568c1e66b73bba
EDS <mightyeaglecorp@gmail.com>
2023-07-23 20:41:56 +0100

    made changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,23 +1,93 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { CircularProgress, Typography } from "@mui/material";
 import {
   BookOutlined,
   CloudDoneOutlined,
   HouseOutlined,
   Laptop,
-  PeopleAltOutlined,
 } from "@mui/icons-material";
+import { httpService } from "../httpService";
 
 function Dashboard() {
+  const [result, setResult] = useState({});
+  const [loading, setLoading] = useState(false);
+  const getData = async () => {
+    setLoading(true);
+    const { data } = await httpService.get("aguila/dashboard");
+
+    if (data) setResult(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <div className="mt-5 mb-5 p-3">
+        <div className="mb-3">
+          <div className="row">
+            <div className="col-lg-3">
+              <Typography variant="h4" fontWeight={300}>
+                DASHBOARD
+              </Typography>
+            </div>
+            <div className="col-lg-3">{loading && <CircularProgress />}</div>
+          </div>
+        </div>
         <div className="row">
-          <TotalExaminations />
-          <TotalCandidates />
-          <TotalSubjects />
-          <TotalCentres />
-          <TotalQuestionBanks />
+          <div className="col-lg-4 mb-4 me-4  border rounded-3 p-3 d-flex justify-content-between align-items-center">
+            <div>
+              <Laptop style={{ height: 50, width: 50 }} />
+              <Typography variant="h6" fontWeight={300}>
+                Examinations
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="h4" fontWeight={300}>
+                {result.examinations}
+              </Typography>
+            </div>
+          </div>
+          <div className="col-lg-4 mb-4 me-4  border rounded-3 p-3 d-flex justify-content-between align-items-center">
+            <div>
+              <BookOutlined style={{ height: 50, width: 50 }} />
+              <Typography variant="h6" fontWeight={300}>
+                Subjects
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="h4" fontWeight={300}>
+                {result.subjects}
+              </Typography>
+            </div>
+          </div>
+          <div className="col-lg-4 mb-4 me-4   border rounded-3 p-3 d-flex justify-content-between align-items-center">
+            <div>
+              <HouseOutlined style={{ height: 50, width: 50 }} />
+              <Typography variant="h6" fontWeight={300}>
+                Centres
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="h4" fontWeight={300}>
+                {result.centres}
+              </Typography>
+            </div>
+          </div>
+          <div className="col-lg-4 mb-4 me-4  border rounded-3 p-3  d-flex justify-content-between align-items-center">
+            <div>
+              <CloudDoneOutlined style={{ height: 50, width: 50 }} />
+              <Typography variant="h6" fontWeight={300}>
+                Question Bank
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="h4" fontWeight={300}>
+                {result.questionBanks}
+              </Typography>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -25,70 +95,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-
-function TotalExaminations() {
-  return (
-    <div className="col-lg-3 mb-4 me-4 shadow-sm p-3 bg-light d-flex justify-content-between align-items-center">
-      <div>
-        <Laptop />
-        <Typography>Examinations</Typography>
-      </div>
-      <div>
-        <Typography>10</Typography>
-      </div>
-    </div>
-  );
-}
-
-function TotalCandidates() {
-  return (
-    <div className="col-lg-3 mb-4 me-4 shadow-sm p-3 bg-light d-flex justify-content-between align-items-center">
-      <div>
-        <PeopleAltOutlined />
-        <Typography>Candidates</Typography>
-      </div>
-      <div>
-        <Typography>10</Typography>
-      </div>
-    </div>
-  );
-}
-function TotalSubjects() {
-  return (
-    <div className="col-lg-3 mb-4 me-4 shadow-sm p-3 bg-light d-flex justify-content-between align-items-center">
-      <div>
-        <BookOutlined />
-        <Typography>Subjects</Typography>
-      </div>
-      <div>
-        <Typography>10</Typography>
-      </div>
-    </div>
-  );
-}
-function TotalCentres() {
-  return (
-    <div className="col-lg-3 mb-4 me-4 shadow-sm p-3 bg-light d-flex justify-content-between align-items-center">
-      <div>
-        <HouseOutlined />
-        <Typography>Centers</Typography>
-      </div>
-      <div>
-        <Typography>10</Typography>
-      </div>
-    </div>
-  );
-}
-function TotalQuestionBanks() {
-  return (
-    <div className="col-lg-3 mb-4 me-4 shadow-sm p-3 bg-light d-flex justify-content-between align-items-center">
-      <div>
-        <CloudDoneOutlined />
-        <Typography>Question Banks</Typography>
-      </div>
-      <div>
-        <Typography>10</Typography>
-      </div>
-    </div>
-  );
-}
